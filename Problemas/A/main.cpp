7553cec8d62d5d2655a55a0f55959c0f3a3cf04c
JavierPC <a01701478@itesm.mx>
2020-09-17 01:53:14 +0000

    A

--- Problemas/A/main.cpp
+++ Problemas/A/main.cpp
@@ -3,13 +3,25 @@
 
 using namespace std;
 
+int formula(int num)
+{
+  int resp;
+  resp=(pow(num,2)-num)/2;
+  return resp;
+}
+
 int main()
 {
-  int m,n,min,max,aux;
+  int m,n,min,max,x,sobra,aux;
   std::cin >> n;
   std::cin >> m;
-  aux=n-m;
-  max=(pow(aux,2)-aux)/2;
-  std::cout<<max;
+  x=n-m+1;
+  max=formula(x);
+  sobra=n%m;
+  aux=m-sobra;
+  x=n/m;
+  min=aux*formula(x)+sobra*formula(x+1);
+  std::cout<<max<<" ";
+  std::cout<<min;
   return 0;
 }
